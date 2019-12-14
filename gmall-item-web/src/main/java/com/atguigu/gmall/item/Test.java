b29c130ebca091406b3c9903c7abe2237b49b97a
15013752489 <395765197@qq.com>
2019-12-14 16:03:30 +0800

    first 2019:12:14 li

---
+++ gmall-item-web/src/main/java/com/atguigu/gmall/item/Test.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.item;
+
+/**
+ * Description:
+ * ClassName: Test
+ * date: 2019/12/14 16:02
+ *
+ * @author fancy Email:395765197@qq.com
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class Test {
+}
