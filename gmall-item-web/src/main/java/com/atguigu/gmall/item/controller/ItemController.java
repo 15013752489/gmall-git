3386bdff655a3e6654fbedbf114512f995c42344
luo <1099717322@qq.com>
2019-12-14 13:56:06 +0800

    罗靖宇提交了一次

---
+++ gmall-item-web/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.item.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Auther luojingyu
+ * @date 2019/12/14 11:49
+ */
+@Controller
+public class ItemController {
+    public void say(){
+        System.out.println("nihao ");
+    }
+}
