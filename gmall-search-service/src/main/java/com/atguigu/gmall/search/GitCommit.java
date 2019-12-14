1ca78a8298dc70a0c0b3a0c6fcbdb23d8ce78ee8
15013752489 <395765197@qq.com>
2019-12-14 12:47:16 +0800

    first init

--- gmall-search-service/src/main/java/com/atguigu/gmall/search/GitCommit.java
+++ gmall-search-service/src/main/java/com/atguigu/gmall/search/GitCommit.java
@@ -13,5 +13,6 @@ public class GitCommit {
     public static void main(String[] args) {
         System.out.println("测试");
         System.out.println("ceshi ");
+        System.out.println("测试");
     }
 }
